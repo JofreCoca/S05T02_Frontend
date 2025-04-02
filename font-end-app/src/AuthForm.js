75c3d4f09db942d6f8dad76bb9af80f80c32dbfa
JofreCoca <jofrecoca@gmail.com>
2025-04-02 13:10:04 +0200

    AuthForms register finish

--- font-end-app/src/AuthForm.js
+++ font-end-app/src/AuthForm.js
@@ -1,3 +1,4 @@
+//AuthFrom.js
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AuthForm.css";
--- font-end-app/src/Notification.js
+++ font-end-app/src/Notification.js
@@ -1,3 +1,4 @@
+//Notification.js
 import React, { useEffect, useState } from "react";
 import "./Notification.css";
 
