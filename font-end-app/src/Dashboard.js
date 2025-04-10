2dbf0fbc721ea599abd7c0dca77be5e13ead5395
JofreCoca <jofrecoca@gmail.com>
2025-04-10 11:05:38 +0200

    AuthForms register finish

--- font-end-app/src/Dashboard.js
+++ font-end-app/src/Dashboard.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
+import user from './uploaders/5user.jpg';
+import dog from './uploaders/5dog.jpg';
+import logoSimple from './logoSimple.png';
 
 const Dashboard = () => {
   const [profile, setProfile] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [photoUrl, setPhotoUrl] = useState(null); // ← NUEVO estado para la imagen
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-  const [email, setEmail] = useState(null)
-  const [isAuthenticated    , setIsAuthenticated] = useState(false)
-  const [errorMessage, setErrorMessage] = ("")
-
-
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-
     if (token) {
-        const decoded = jwtDecode(token)
-        setEmail(decoded.user)
-        setIsAuthenticated(true)
-
-
+      const decoded = jwtDecode(token);
+      setEmail(decoded.user);
+      setPhotoUrl(decoded.photo_url); // ← Obtener photo_url del token
+      setIsAuthenticated(true);
     } else {
       setErrorMessage("No token found in localStorage");
-       setTimeout(() => {}, 2000);
-      navigate("/"); // Redirigir al login si no hay token
+      setTimeout(() => {}, 2000);
+      navigate("/");
     }
   }, [navigate]);
 
@@ -35,28 +35,31 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard-wrapper">
-    {errorMessage && <h1>{errorMessage}</h1>}
+      {errorMessage && <h1>{errorMessage}</h1>}
+
       <div className="logout-container">
+      <img src={logoSimple} alt="Perfil" className="profile-logoSimple" />
+        <h1 className="welcome-text">Bienvenido, {email || profile?.ownerMail}</h1>
         <button className="logout-button" onClick={handleLogout}>
           Cerrar Sesión
         </button>
       </div>
-
-      {isAuthenticated ? (
-        <h1>Bienvenido, {email || profile?.ownerMail}</h1>
-      ) : (
-        <h1>Cargando perfil...</h1>
-      )}
+        <div className="photo-container">
+          <img src={user} alt="Perfil" className="profile-photo-user" />
+        </div>
+        <div className="photo-container">
+          <img src={dog} alt="Perfil" className="profile-photo-dog" />
+        </div>
 
       <div className="dashboard-container">
         {/* Aquí puedes mostrar el resto de la información del usuario */}
       </div>
-
     </div>
   );
 };
 
 export default Dashboard;
+
 
 
 
